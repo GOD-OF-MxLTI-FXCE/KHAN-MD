52c3225751513f6fdd142388c8b40547e3974a9f
GOD-OF-MxLTI-FXCE <godofmultifxce@gmail.com>
2025-07-31 13:23:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/7zfdcq.jpg",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || "🤌",
+PREFIX: process.env.PREFIX || "?",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "𝐊𝐈𝐍𝐆𝐀𝐍𝐃𝐐𝐔𝐄𝐍𝐍",
 // add bot namw here for menu
